af77b36610431d4271d2a7ac7d1c4c847876713d
Grant Yi <imgrantyi@gmail.com>
2024-11-07 08:59:29 -0800

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ export default defineConfig({
   plugins: [],
   server: { port: 8000, open: true, cors: true },
   clearScreen: false,
-  base: process.env.NODE_ENV === 'production' ? 'nyt-union-stacker' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/nyt-union-stacker' : '/',
 });
